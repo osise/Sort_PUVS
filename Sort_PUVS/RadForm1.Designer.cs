fdfb057bf472c525d6dea975102202c227c0eca5
S_O <spacecore66@gmail.com>
2021-06-23 15:16:20 +0500

    Uniq

--- Sort_PUVS/RadForm1.Designer.cs
+++ Sort_PUVS/RadForm1.Designer.cs
@@ -29,20 +29,161 @@ namespace Sort_PUVS
         /// </summary>
         private void InitializeComponent()
         {
+            this.radOpenFileDialog1 = new Telerik.WinControls.UI.RadOpenFileDialog();
+            this.radRichTextEditor1 = new Telerik.WinControls.UI.RadRichTextEditor();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.startButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new Telerik.WinControls.UI.RadProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.radRichTextEditor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
+            // radRichTextEditor1
+            // 
+            this.radRichTextEditor1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(189)))), ((int)(((byte)(232)))));
+            this.radRichTextEditor1.Location = new System.Drawing.Point(214, 13);
+            this.radRichTextEditor1.Name = "radRichTextEditor1";
+            this.radRichTextEditor1.SelectionFill = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(78)))), ((int)(((byte)(158)))), ((int)(((byte)(255)))));
+            this.radRichTextEditor1.Size = new System.Drawing.Size(498, 435);
+            this.radRichTextEditor1.TabIndex = 0;
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(13, 13);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(180, 41);
+            this.radButton1.TabIndex = 1;
+            this.radButton1.Text = "Загрузить";
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(12, 72);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(180, 41);
+            this.radButton2.TabIndex = 2;
+            this.radButton2.Text = "Открыть папку с файлами";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(13, 344);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(180, 41);
+            this.radButton3.TabIndex = 3;
+            this.radButton3.Text = "Посмотреть логи";
+            // 
+            // radButton4
+            // 
+            this.radButton4.Location = new System.Drawing.Point(13, 407);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(180, 41);
+            this.radButton4.TabIndex = 4;
+            this.radButton4.Text = "Выход";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 233);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "button1";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(117, 233);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 6;
+            this.cancelButton.Text = "button2";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 299);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(180, 24);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Text = "radProgressBar1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 165);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(112, 165);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
             // RadForm1
-            //
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 460);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.radButton4);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.radRichTextEditor1);
             this.Name = "RadForm1";
-            this.Text = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "richTextEditorRibbonBar1";
+            ((System.ComponentModel.ISupportInitialize)(this.radRichTextEditor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Telerik.WinControls.UI.RadOpenFileDialog radOpenFileDialog1;
+        private Telerik.WinControls.UI.RadRichTextEditor radRichTextEditor1;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button cancelButton;
+        private Telerik.WinControls.UI.RadProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
--- Sort_PUVS/RadForm1.cs
+++ Sort_PUVS/RadForm1.cs
@@ -8,14 +8,232 @@ using System.Text;
 using System.Windows.Forms;
 using Telerik.WinControls;
 using Telerik.WinControls.UI;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace Sort_PUVS
 {
     public partial class RadForm1 : Telerik.WinControls.UI.RadForm
     {
+        CancellationTokenSource cts; // Источник токена отмены
+        DataSet ds = new DataSet();
+      
         public RadForm1()
         {
             InitializeComponent();
         }
+
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            cts = new CancellationTokenSource();
+            startButton.Enabled = false;
+            cancelButton.Enabled = true;
+            progressBar1.Value1 = 0;
+            var progress = new Progress<int>(ProgressHandler);
+            try
+            {
+                await WorkAsync(cts.Token, progress);
+            }
+            catch (OperationCanceledException ex)
+            {
+                MessageBox.Show("Операция прервана.", "Внимание.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            startButton.Enabled = true;
+            cancelButton.Enabled = false;
+        }
+
+        private void ProgressHandler(int number)
+        {
+            progressBar1.Value1 = number;
+        }
+
+        void texty(int i)
+        {
+            radRichTextEditor1.Text = "загружено " + i + " процентов";
+        }
+        private async Task WorkAsync(CancellationToken token, IProgress<int> progress)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                token.ThrowIfCancellationRequested();
+                await Task.Delay(100);
+                progress?.Report(i);
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            cts?.Cancel();
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                radRichTextEditor1.Text = fbd.SelectedPath;
+            }
+        }
+
+        public class ExcelFile
+        {
+            string XlFile = null;
+            string XlFile1 = null;
+            Excel.Application myExcelApplication;
+            Excel.Workbook myExcelWorkbook;
+            Excel.Worksheet myExcelWorkSheet;
+
+            public string ExcelFilePath { get; set; } = string.Empty;
+
+            public int Rownumber { get; set; } = 1;
+
+            public void UniqueEx()
+            { }
+            public void OpenExcel()
+            {
+                myExcelApplication = null;
+
+                myExcelApplication = new Excel.Application
+                {
+                    DisplayAlerts = false // turn off alerts
+                }; // create Excell App
+
+
+                myExcelWorkbook = myExcelApplication.Workbooks._Open(ExcelFilePath, System.Reflection.Missing.Value,
+                   System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                   System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                   System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                   System.Reflection.Missing.Value, System.Reflection.Missing.Value); // open the existing excel file
+
+                int numberOfWorkbooks = myExcelApplication.Workbooks.Count; // get number of workbooks (optional)
+
+                myExcelWorkSheet = (Excel.Worksheet)myExcelWorkbook.Worksheets[1]; // define in which worksheet, do you want to add data
+                myExcelWorkSheet.Name = "Лист 1"; // define a name for the worksheet (optinal)
+
+                int numberOfSheets = myExcelWorkbook.Worksheets.Count; // get number of worksheets (optional)
+
+                MessageBox.Show("Success");
+            }
+
+            public void AddDataToExcel(string index, int stolb, int row)
+            {
+                myExcelWorkSheet.Cells[row, stolb] = index;
+            }
+
+            public void CloseExcel()
+            {
+                try
+                {
+                    myExcelWorkbook.SaveAs(ExcelFilePath, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                                                   System.Reflection.Missing.Value, System.Reflection.Missing.Value, Excel.XlSaveAsAccessMode.xlNoChange,
+                                                   System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                                                   System.Reflection.Missing.Value, System.Reflection.Missing.Value); // Save data in excel
+
+
+                    myExcelWorkbook.Close(true, ExcelFilePath, System.Reflection.Missing.Value); // close the worksheet
+                }
+                finally
+                {
+                    //  if (myExcelApplication != null)
+                    // {
+
+                    myExcelApplication.Quit(); // close the excel application
+                    GC.Collect();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(myExcelWorkSheet);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(myExcelWorkbook);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(myExcelApplication);
+                    // }
+                }
+
+            }
+
+            private DataTable GetTableDataFromXl(string XlFile)
+            {
+                DataTable dt = new DataTable();
+                try
+                {
+                    string Ext = Path.GetExtension(XlFile);
+                    string connectionString = "";
+                    if (Ext == ".xls")
+                    {   //For Excel 97-03
+                        connectionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source =" + XlFile + "; Extended Properties = 'Excel 8.0;HDR=YES'";
+                    }
+                    else if (Ext == ".xlsx")
+                    {    //For Excel 07 and greater
+                        connectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source =" + XlFile + "; Extended Properties = 'Excel 8.0;HDR=YES'";
+                    }
+                    OleDbConnection conn = new OleDbConnection(connectionString);
+                    OleDbCommand cmd = new OleDbCommand();
+                    OleDbDataAdapter dataAdapter = new OleDbDataAdapter();
+
+                    cmd.Connection = conn;
+                    //Fetch 1st Sheet Name
+                    conn.Open();
+                    DataTable dtSchema;
+                    dtSchema = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    string ExcelSheetName = dtSchema.Rows[0]["TABLE_NAME"].ToString();
+                    conn.Close();
+                    //Read all data of fetched Sheet to a Data Table
+                    conn.Open();
+                    cmd.CommandText = "SELECT * From [" + ExcelSheetName + "]";
+                    dataAdapter.SelectCommand = cmd;
+                    dataAdapter.Fill(dt);
+                    conn.Close();
+                }
+                catch (Exception ex)
+                { MessageBox.Show(Convert.ToString(ex)); }
+
+                return dt;
+            }
+
+            private void LoadData()
+            {
+                ExcelFile excelFile = new ExcelFile();
+                try
+                {
+                    //string XlFile1 = null;
+                    //string XlFile = null;
+                    DataTable dtInd = GetTableDataFromXl(XlFile); // Converting Excel Data into DataTable 
+                   
+                    DataTable dtPens = GetTableDataFromXl(XlFile1);
+                    excelFile.ExcelFilePath = XlFile1;
+                    excelFile.OpenExcel();
+                    MessageBox.Show("1");
+
+                   // progressBar1.Maximum = dtPens.Rows.Count * dtInd.Rows.Count;
+
+
+                    for (int y = 0; y < dtPens.Rows.Count; y++)
+                    {
+                        for (int i = 0; i < dtInd.Rows.Count; i++)
+                        {
+                            // progressBar1.Value = progressBar1.Maximum - 1;
+
+                            if (Convert.ToString(dtInd.Rows[i][1]) == Convert.ToString(dtPens.Rows[y][6])) // откуда - куда
+                            { //MessageBox.Show("Found");
+                                excelFile.AddDataToExcel("есть", 8, y + 3); //куда пишем
+                            }
+                            //label15.Text = Convert.ToString(i);
+                        }
+                        //   label14.Text = Convert.ToString(y);
+
+                    }
+                    excelFile.CloseExcel();
+                    MessageBox.Show("Found");
+                }
+                //(dtPens.Rows[6] in dtInd.Rows[1])
+
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(Convert.ToString(ex));
+                }
+            }
+        }
     }
 }
+
